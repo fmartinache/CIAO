1321aedd827318016f4c34cd56b92dde327787bb
Frantz Martinache <frantz.martinache@oca.eu>
2019-10-18 16:32:08 +0200

    exposure time fix and updated documentation

--- control/ciao_cam.py
+++ control/ciao_cam.py
@@ -21,7 +21,6 @@ class Cam():
         if os.path.exists(self.fifo_in):
             try:
                 self.cmd_fifo = open(self.fifo_in, 'w')
-                #self.inf_fifo = open(self.fifo_out, 'r')
                 self.connected = True
             except:
                 print("could not open the fifo in write mode")
@@ -29,18 +28,21 @@ class Cam():
             print("expected fifo does not exist")
 
         # exposure time control: only a finite number of possibilities
-        self.cam_tints = [0.00001, 0.00002, 0.00005,
+        self.cam_tints = [0.00001, #0.00002, 0.00005,
                           0.00010, 0.00020, 0.00050,
                           0.00100, 0.00200, 0.00500, 
                           0.01000, 0.02000, 0.05000,
                           0.10000, 0.20000, 0.50000]
 
+        self.cam_tint = 0.0002 # fall back on 0.2 ms
+        self.cam_itint = self.cam_tints.index(self.cam_tint)
+            
         if self.connected:
-            try:
-                self.get_tint()
-            except:
-                self.cam_tint = 0.001 # fall back on this value
-
+            self.cam_tint = 0.0002 # fall back on 0.2 ms
+            #try:
+            #self.get_tint()
+            #except:
+                
             #pdb.set_trace()
             self.cam_itint = self.cam_tints.index(self.cam_tint)
             #self.set_tint(self.cam_tints[self.cam_itint])
@@ -85,13 +87,16 @@ class Cam():
             self.cmd_fifo.write("tint?")
             self.cmd_fifo.flush()
             time.sleep(0.5)
-            tmp = float(self.inf_fifo.read())
-            #print tmp
+            self.inf_fifo = open(self.fifo_out, 'r')
+            with open(self.fifo_out, 'r') as fifo:
+                tmp = float(fifo.read())
+                print("integration time read:", tmp)
             self.cam_tint = tmp # assign tint to current structure
 
     # =========================================================================
     def tint_dec(self,):
         self.cam_itint = max(self.cam_itint-1, 0)
+        print("!!!!!!", self.cam_itint)
         self.cam_tint = self.cam_tints[self.cam_itint]
         if self.connected:
             self.set_tint(self.cam_tint)
@@ -99,6 +104,7 @@ class Cam():
     # =========================================================================
     def tint_inc(self,):
         self.cam_itint = min(self.cam_itint+1, len(self.cam_tints)-1)
+        print("!!!!!!", self.cam_itint)
         self.cam_tint = self.cam_tints[self.cam_itint]
         if self.connected:
             self.set_tint(self.cam_tint)
